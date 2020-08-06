bbfee0364a25cd890ec5b35906b5e539f5db673c
anastasiia-kolieva <nastya1110@gmail.com>
2020-08-06 14:34:26 +0300

    made task-1

--- js/tasks.js
+++ js/tasks.js
@@ -1,13 +1,15 @@
+"use strict";
+
+import users from "./users.js";
+
 // TASK 1
 // Получить массив имен всех пользователей (поле name).
 
 const getUserNames = users => {
-  // твой код
-  const names=users.map(function(user){
-      return user.name;
-  })
+  return users.name;
 };
-// Проверить работает ли код!!!!
 
-console.log(getUserNames(users));
+const userNames = users.map(getUserNames);
+
+console.log(userNames);
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
