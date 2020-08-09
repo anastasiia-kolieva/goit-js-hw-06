412fee15c3126425611d0101d3ad376e1f199080
anastasiia-kolieva <nastya1110@gmail.com>
2020-08-09 23:34:44 +0300

    made task-2

--- js/tasks.js
+++ js/tasks.js
@@ -5,7 +5,7 @@ import users from "./users.js";
 // TASK 1
 // Получить массив имен всех пользователей (поле name).
 
-const getUserNames = users => {
+const getUserNames = (users) => {
   return users.name;
 };
 
@@ -13,3 +13,12 @@ const userNames = users.map(getUserNames);
 
 console.log(userNames);
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+// TASK 2
+// Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+
+const getUsersWithEyeColor = (users, color) => {
+  return users.filter((users) => users.eyeColor === color);
+};
+
+console.log(getUsersWithEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
