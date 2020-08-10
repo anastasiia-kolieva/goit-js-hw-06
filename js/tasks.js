80eab87f1f8660dd43a62a06477947b39a117de7
anastasiia-kolieva <nastya1110@gmail.com>
2020-08-10 19:38:46 +0300

    made task 8

--- js/tasks.js
+++ js/tasks.js
@@ -70,3 +70,13 @@ const calculateTotalBalance = (users) => {
 };
 
 console.log(calculateTotalBalance(users)); // 20916
+
+// TASK 8
+// Массив имен всех пользователей у которых есть друг с указанным именем.
+
+const getUsersWithFriend = (users, friendName) => {
+return users.filter(user=>user.friends.includes(friendName)).map(user=>user.name);
+};
+
+console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
