ce5af6c70684497e5839117384317a5eb9da9e83
anastasiia-kolieva <nastya1110@gmail.com>
2020-08-10 13:14:05 +0300

    made task 5

--- js/tasks.js
+++ js/tasks.js
@@ -40,3 +40,14 @@ const getInactiveUsers = (users) => {
 };
 
 console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+
+// TASK 5
+// Получить пользоваля (не массив) по email (поле email, он уникальный).
+
+const getUserWithEmail = (users, email) => {
+  return users.find(user=> user.email===email);
+};
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
