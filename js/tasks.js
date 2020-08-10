e2a706f01b31849b8d3be212d9c1088b8e084863
anastasiia-kolieva <nastya1110@gmail.com>
2020-08-10 23:13:10 +0300

    made task 9

--- js/tasks.js
+++ js/tasks.js
@@ -80,3 +80,13 @@ return users.filter(user=>user.friends.includes(friendName)).map(user=>user.name
 
 console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+
+// TASK 9
+// Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+
+const getNamesSortedByFriendsCount = users => {
+  return users.sort((a,b)=>a.friends.length-b.friends.length).map(user=>user.name);  
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
