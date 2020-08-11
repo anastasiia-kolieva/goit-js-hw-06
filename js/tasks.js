fedb44b66c7aaed661961275f7ff39b3ccfb011c
anastasiia-kolieva <nastya1110@gmail.com>
2020-08-11 14:18:13 +0300

    made task 10

--- js/tasks.js
+++ js/tasks.js
@@ -75,7 +75,9 @@ console.log(calculateTotalBalance(users)); // 20916
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
 const getUsersWithFriend = (users, friendName) => {
-return users.filter(user=>user.friends.includes(friendName)).map(user=>user.name);
+  return users
+    .filter((user) => user.friends.includes(friendName))
+    .map((user) => user.name);
 };
 
 console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
@@ -84,9 +86,24 @@ console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sher
 // TASK 9
 // Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
 
-const getNamesSortedByFriendsCount = users => {
-  return users.sort((a,b)=>a.friends.length-b.friends.length).map(user=>user.name);  
+const getNamesSortedByFriendsCount = (users) => {
+  return users
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map((user) => user.name);
 };
 
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+// TASK 10
+// Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+
+const getSortedUniqueSkills = (users) => {
+  return users
+    .reduce((acc, user) => [...acc, ...user.skills], [])
+    .filter((skill, index, array) => array.indexOf(skill) === index)
+    .sort();
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proi
